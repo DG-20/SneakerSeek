6c1910bbb744a33fce2fce6431519d4c01bd4ba4
Divyansh Goyal <divyansh.goyal2001@gmail.com>
2022-03-22 14:37:31 -0600

    Added stuff

--- SneakerSeek/app/views.py
+++ SneakerSeek/app/views.py
@@ -22,3 +22,42 @@ def index(request):
             return redirect("index")
     else:
         return render(request, "index.html")
+
+
+def register(request):
+    if request.method == "POST":
+        fname = request.POST["f_name"]
+        lname = request.POST["l_name"]
+        username = request.POST["username"]
+        password = request.POST["pass"]
+        verifypass = request.POST["confirmpass"]
+        email = request.POST["email"]
+
+        if password == verifypass:
+            # User is the thing being imported, filter goes in the db and checks
+            # if the email entered already exists.
+            if User.objects.filter(email=email).exists():
+                messages.info(
+                    request, "Email already in use! Please go back and log in!"
+                )
+                # Send em back to register
+                return redirect("register")
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, "Username not available! Try a different one!")
+                return redirect("register")
+            else:
+                user = User.objects.create_user(
+                    username=username,
+                    email=email,
+                    password=password,
+                    first_name=fname,
+                    last_name=lname,
+                )
+                user.save()
+                return redirect("login")
+        else:
+            messages.info(request, "Passwords did not match!")
+            return redirect("register")
+
+    else:
+        return render(request, "register.html")
